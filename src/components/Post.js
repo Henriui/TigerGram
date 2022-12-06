58cd970a491febd98433cb9e8cedc559a8ef77ec
Jonne <clownfiesta98@gmail.com>
2022-12-06 22:03:59 +0200

    Group-Assinment

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,7 +4,8 @@ import { ListItem, ListItemText, ListItemAvatar, Avatar, TextField, Button } fro
 import "../styles/Post.css"
 import Comments from './Comments';
 import SendIcon from '@material-ui/icons/Send'
-import HoverRating from './HoverRating';
+import DeleteIcon from '@mui/icons-material/Delete';
+// import HoverRating from './HoverRating';
 
 function Post({ post }) {
     const [comment, setComment] = useState("");
@@ -17,10 +18,12 @@ function Post({ post }) {
     // FIXME: when submitting comment, the text field does not empty out.
     const addComment = (event) => {
         event.preventDefault();
-        const comments = [...post.comments, { username: "Testi Kommentoija", avatar: "/static/images/avatar/2.jpg", text: comment }]
-        const newPostComment = { ...post, comments }
-        setComment("");
-        Object.assign(post, newPostComment)
+        if (comment !== "") {
+            const comments = [...post.comments, { username: "Testi Kommentoija", avatar: "/static/images/avatar/2.jpg", text: comment }]
+            const newPostComment = { ...post, comments }
+            setComment("");
+            Object.assign(post, newPostComment)
+        } 
     }
     return (
         <div className='post'>
@@ -30,6 +33,9 @@ function Post({ post }) {
                         <Avatar className="post_avatar" src={post.avatar} alt="avatar"></Avatar>
                     </ListItemAvatar>
                     <ListItemText primary={post.username} secondary="Abdi Danhi, UEA"></ListItemText>
+                    <Button className='delete_button' variant="outlined" startIcon={<DeleteIcon />} onClick={() => console.log("pls delete me :(")}>
+                        <p className='delete_text'>Delete</p>
+                    </Button>
                 </ListItem>
                 <img className='post_image' src={post.image} alt='Post pic' />
                 <h4 className='post_text'><strong>{post.username}: </strong>{post.text} </h4>
